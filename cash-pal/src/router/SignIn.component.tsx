954cef94fdd5f54e37ffdf20e7c2860661e33904
Raul Haro <raulharo99@gmail.com>
2023-09-07 12:33:54 -0400

    Created the SignInForm component and added it to the SignIn page

--- cash-pal/src/router/SignIn.component.tsx
+++ cash-pal/src/router/SignIn.component.tsx
@@ -1,10 +1,12 @@
 import RegistrationForm from "../components/register components/RegistrationForm.component";
+import SignInForm from "../components/sign in components/SignInForm.component";
 
 const SignIn = () => {
   return (
     <div>
       <h1>Sign In Page</h1>
       <RegistrationForm />
+      <SignInForm />
     </div>
   )
 }
--- cash-pal/src/utilities/UserUtils.tsx
+++ cash-pal/src/utilities/UserUtils.tsx
@@ -1,4 +1,6 @@
 import axios from "axios";
+import store from "../store";
+import { setToken } from "../features/account";
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 
@@ -7,19 +9,38 @@ const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
 // };
 
 export const register = async (username: string, password: string) => {
-    try {
-      const response = await axios.post(`http://localhost:8080/register`, {
-        username: username,
-        password: password,
-      });
-      if (response.status === 201 || response.status === 200) {
-        console.log("Successfully registered as " + username);
-      } else {
-        throw new Error("Failed to register");
-      }
-    } catch (error) {
-      console.log("Testing"); 
-      console.error(error);
+  try {
+    const response = await axios.post(`http://localhost:8080/register`, {
+      username: username,
+      password: password,
+    });
+    if (response.status === 201 || response.status === 200) {
+      console.log("Successfully registered as " + username);
+    } else {
       throw new Error("Failed to register");
     }
+  } catch (error) {
+    console.log("Testing");
+    console.error(error);
+    throw new Error("Failed to register");
+  }
+};
+
+export const fetchSignInDetails = async (username: string, password: string) => {
+  try {
+    const response = await axios.post(`http://localhost:8080/login`, {
+      username,
+      password,
+    });
+    if (response.status === 200) {
+      const { token } = response.data;
+      store.dispatch(setToken(token));
+    } else {
+      throw new Error("Failed to log in");
+    }
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to log in");
+  }
 };
