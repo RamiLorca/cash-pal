09c65036b7b9a3510fac9f972ebce7e116c03622
Raul Haro <raulharo99@gmail.com>
2023-09-23 20:49:58 -0400

    Added functionality to save amount entered in transaction form to a useState hook.

--- cash-pal/src/components/home components/TransactionForm.component.tsx
+++ cash-pal/src/components/home components/TransactionForm.component.tsx
@@ -3,82 +3,91 @@ import { transactionRequest } from '../../utilities/TransferUtils';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store';
 
-const { account_id } = useSelector((state: RootState) => ({
-  account_id: state.account.account_id,
-}));
-
 const TransactionForm = () => {
 
-    const [activeButton, setActiveButton] = useState('Send Money');
+  const { account_id } = useSelector((state: RootState) => ({
+    account_id: state.account.account_id,
+  }));
 
-    const handleButtonClick = (buttonType: string) => {
-        setActiveButton(buttonType);
+  const [activeButton, setActiveButton] = useState('Send Money');
+  const [amount, setAmount] = useState(0.00);
+
+  const handleButtonClick = (buttonType: string) => {
+    setActiveButton(buttonType);
+  };
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (activeButton === 'Send Money') {
+      try {
+        // Logic for sending money
+        const response = await transactionRequest(account_id, receiverId, amount);
+        console.log(response);
+        // Add setter functions to clear input fields.
+
+        /*
+         * Send request to the backend
+         * If (success status returned)
+         *   Send POST to the back end to store data in the database
+         *  
+        */
+        console.log('Sending money...');
+      }
+      catch (error) {
+        console.error(error);
       };
+    } else if (activeButton === 'Request Money') {
+      try {
+        // Logic for sending money
+        const response = await transactionRequest(senderId, account_id, amount);
+        console.log(response);
+        // Add setter functions to clear input fields.
 
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        if (activeButton === 'Send Money') {
-          try {
-            // Logic for sending money
-          const response = await transactionRequest(account_id, receiverId, amount);
-          console.log(response);
-          // Add setter functions to clear input fields.
-
-          /*
-           * Send request to the backend
-           * If (success status returned)
-           *   Send POST to the back end to store data in the database
-           *  
-          */
-          console.log('Sending money...');
-          }
-          catch (error) {
-            console.error(error);
-          };
-        } else if (activeButton === 'Request Money') {
-          try {
-            // Logic for sending money
-          const response = await transactionRequest(senderId, account_id, amount);
-          console.log(response);
-          // Add setter functions to clear input fields.
-
-          /*
-           * Send request to the backend
-           * If (success status returned)
-           *   Send POST to the back end to store data in the database
-           *  
-          */
-          console.log('Requesting money...');
-          }
-          catch (error) {
-            console.error(error);
-          };
-        }
+        /*
+         * Send request to the backend
+         * If (success status returned)
+         *   Send POST to the back end to store data in the database
+         *  
+        */
+        console.log('Requesting money...');
+      }
+      catch (error) {
+        console.error(error);
       };
+    }
+  };
 
-    return (
-      <div>
-        <h1>Transaction Form</h1>
+  return (
+    <div>
+      <h1>Transaction Form</h1>
 
-        <button onClick={() => handleButtonClick('Send Money')}>Send Money</button>
-        <button onClick={() => handleButtonClick('Request Money')}>Request Money</button>
+      <button onClick={() => handleButtonClick('Send Money')}>Send Money</button>
+      <button onClick={() => handleButtonClick('Request Money')}>Request Money</button>
 
-        <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
 
-            <input type="text" placeholder="Enter username"></input>
+        <input type="text" placeholder="Enter username"></input>
 
-            <br/>
+        <br />
 
-            <input type="number" min="0.00" step="0.01" placeholder="Enter cash amount"></input>
+        <input
+          type="number"
+          min="0.00"
+          step="0.01"
+          placeholder="Enter cash amount"
+          onChange={(event) => {
+            setAmount(event.target.valueAsNumber);
+          }}
+        />
 
-            <br/>
+        <br />
 
-            <input type="submit" value={activeButton} />
-    
-        </form>
+        <input type="submit" value={activeButton} />
 
-      </div>
-    )
-  }
-  
+      </form>
+
+    </div>
+  )
+}
+
 export default TransactionForm;
